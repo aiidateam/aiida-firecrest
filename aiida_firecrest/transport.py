b2b7a3947aa44b4a4d7d6c9fb3e7cd1aa80cef0a
Chris Sewell <chrisj_sewell@hotmail.com>
2021-12-04 09:30:37 +0100

    🧪 TESTS: Add test infrastructure

--- aiida_firecrest/transport.py
+++ aiida_firecrest/transport.py
@@ -7,19 +7,139 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Transport interface."""
+import posixpath
+from pathlib import Path, PurePosixPath
+from typing import Any, List, Optional, Tuple, Type, Union
+
+import firecrest as f7t
+from aiida.cmdline.params.options.overridable import OverridableOption
+from aiida.cmdline.params.types.path import AbsolutePathOrEmptyParamType
 from aiida.transports import Transport
+from click.types import ParamType
+from firecrest.FirecrestException import HeaderException
+
+try:
+    from typing import TypedDict  # type: ignore
+except ImportError:
+    from typing_extensions import TypedDict
+
+
+class ValidAuthOption(TypedDict, total=False):  # type: ignore
+    option: Optional[OverridableOption]  # existing option
+    switch: bool  # whether the option is a boolean flag
+    type: Union[Type[Any], ParamType]  # noqa: A003
+    default: Any
+    non_interactive_default: bool  # whether option should provide a default in non-interactive mode
+    prompt: str  # for interactive CLI
+    help: str  # noqa: A003
+
+
+def login_decorator(func):
+    def _decorator(self, *args, **kwargs):
+        return self.keycloak.account_login(func)(self, *args, **kwargs)
+
+    return _decorator
+
+
+class FirecrestAuthorizationClass:
+    def __init__(self, token_uri: str, client_id: str, client_secret: str):
+        self.keycloak = f7t.ClientCredentialsAuthorization(
+            client_id,
+            client_secret,
+            token_uri,
+        )
+
+    # TODO maybe only check if token is valid on Transport.open? (rather than on every call)
+    @login_decorator
+    def get_access_token(self):
+        return self.keycloak.get_access_token()
 
 
 class FirecrestTransport(Transport):
     """Transport interface for FirecREST."""
 
+    _valid_auth_options: List[Tuple[str, dict]] = [
+        (
+            "url",
+            {
+                "type": str,
+                "non_interactive_default": True,
+                "prompt": "Server URL",
+                "help": "URL to FirecREST server",
+            },
+        ),
+        (
+            "token_uri",
+            {
+                "type": str,
+                "non_interactive_default": True,
+                "prompt": "Token URI",
+                "help": "URI for retrieving FirecREST authentication tokens",
+            },
+        ),
+        (
+            "client_id",
+            {
+                "type": str,
+                "non_interactive_default": True,
+                "prompt": "Client ID",
+                "help": "FirecREST client ID",
+            },
+        ),
+        (
+            "secret_path",
+            {
+                "type": AbsolutePathOrEmptyParamType(dir_okay=False, exists=True),
+                "non_interactive_default": True,
+                "prompt": "Secret key file",
+                "help": "Absolute path to file containing FirecREST client secret",
+            },
+            # TODO: format of secret file, and lookup secret by default in ~/.firecrest/secrets.json
+        ),
+    ]
+
+    def __init__(
+        self,
+        *,
+        url: str,
+        token_uri: str,
+        client_id: str,
+        secret_path: str,
+        machine: str,
+        **kwargs: Any,
+    ):
+        super().__init__()
+        self._machine = machine
+        self._url = url
+        self._token_uri = token_uri
+        self._client_id = client_id
+        self._secret_path = secret_path
+
+        secret = Path(self._secret_path).read_text()
+
+        self._client = f7t.Firecrest(
+            firecrest_url=self._url,
+            authorization=FirecrestAuthorizationClass(
+                token_uri=self._token_uri, client_id=client_id, client_secret=secret
+            ),
+        )
+
+        self._cwd = PurePosixPath()
+
+    def _get_path(self, path: str) -> str:
+        return posixpath.normpath(self._cwd.joinpath(path))
+
     def open(self):  # noqa: A003
-        raise NotImplementedError
+        # TODO allow for batch connections in pyfirecrest?
+        pass
 
     def close(self):
-        raise NotImplementedError
+        pass
 
-    def chdir(self, path):
+    def getcwd(self) -> str:
+        return str(self._cwd)
+
+    def chdir(self, path: str) -> None:
         raise NotImplementedError
 
     def chmod(self, path, mode):
@@ -49,26 +169,50 @@ class FirecrestTransport(Transport):
     def gettree(self, remotepath, localpath, *args, **kwargs):
         raise NotImplementedError
 
-    def getcwd(self):
-        raise NotImplementedError
+    # TODO: once https://github.com/eth-cscs/firecrest/pull/133 is deployed,
+    # then this can be used for get_attribute, isdir, isfile, etc
 
     def get_attribute(self, path):
         raise NotImplementedError
 
-    def isdir(self, path):
-        raise NotImplementedError
+    def isdir(self, path: str) -> bool:
+        try:
+            ftype = self._client.file_type(self._machine, self._get_path(path))
+        except HeaderException as exc:
+            if "X-Invalid-Path" in exc._responses[-1].headers:
+                return False
+            raise
+        return ftype == "directory"
 
-    def isfile(self, path):
-        raise NotImplementedError
+    def isfile(self, path: str) -> bool:
+        try:
+            ftype = self._client.file_type(self._machine, self._get_path(path))
+        except HeaderException as exc:
+            if "X-Invalid-Path" in exc.responses[-1].headers:
+                return False
+            raise
+        return ftype != "directory"
 
-    def listdir(self, path=".", pattern=None):
-        raise NotImplementedError
+    def listdir(self, path: str = ".", pattern: Optional[str] = None) -> List[str]:
+        if pattern is not None:
+            raise NotImplementedError("pattern matching")
+        try:
+            output = self._client.list_files(
+                self._machine, self._get_path(path), showhidden=True
+            )
+        except HeaderException as exc:
+            if "X-Invalid-Path" in exc.responses[-1].headers:
+                raise FileNotFoundError(self._get_path(path))
+            raise
+        return [data["name"] for data in output]
 
-    def makedirs(self, path, ignore_existing=False):
-        raise NotImplementedError
+    # TODO handle ignore_existing and failure modes for makedir/makedirs
 
-    def mkdir(self, path, ignore_existing=False):
-        raise NotImplementedError
+    def makedirs(self, path: str, ignore_existing: bool = False) -> None:
+        self._client.mkdir(self._machine, self._get_path(path), p=True)
+
+    def mkdir(self, path: str, ignore_existing: bool = False) -> None:
+        self._client.mkdir(self._machine, self._get_path(path), p=False)
 
     def put(self, localpath, remotepath, *args, **kwargs):
         raise NotImplementedError
@@ -97,5 +241,11 @@ class FirecrestTransport(Transport):
     def symlink(self, remotesource, remotedestination):
         raise NotImplementedError
 
-    def path_exists(self, path):
-        raise NotImplementedError
+    def path_exists(self, path: str) -> bool:
+        try:
+            self._client.file_type(self._machine, self._get_path(path))
+        except HeaderException as exc:
+            if "X-Invalid-Path" in exc.responses[-1].headers:
+                return False
+            raise
+        return True
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,5 +6,46 @@ def test_init_scheduler():
     FirecrestScheduler()
 
 
-def test_init_transport():
-    FirecrestTransport()
+def init_transport(firecrest_server):
+    transport = FirecrestTransport(
+        url=firecrest_server.url,
+        token_uri=firecrest_server.token_uri,
+        client_id=firecrest_server.client_id,
+        secret_path=firecrest_server.secret_path,
+        machine=firecrest_server.machine,
+    )
+    return transport
+
+
+def test_init_transport(firecrest_server):
+    init_transport(firecrest_server)
+
+
+def test_path_exists(firecrest_server):
+    transport = init_transport(firecrest_server)
+    assert transport.path_exists(firecrest_server.scratch_path)
+    assert not transport.path_exists(firecrest_server.scratch_path + "/file.txt")
+
+
+def test_isdir(firecrest_server):
+    transport = init_transport(firecrest_server)
+    assert transport.isdir(firecrest_server.scratch_path)
+    assert not transport.isdir(firecrest_server.scratch_path + "/other")
+
+
+def test_mkdir(firecrest_server):
+    transport = init_transport(firecrest_server)
+    transport.mkdir(firecrest_server.scratch_path + "/test")
+    assert transport.isdir(firecrest_server.scratch_path + "/test")
+
+
+def test_isfile(firecrest_server):
+    transport = init_transport(firecrest_server)
+    assert not transport.isfile(firecrest_server.scratch_path + "/file.txt")
+    # TODO make file then re-test
+
+
+def test_listdir(firecrest_server):
+    transport = init_transport(firecrest_server)
+    assert transport.listdir(firecrest_server.scratch_path) == []
+    # TODO make file/folder then re-test
