d99ae56ef0c381699807ab08499b14a1c1537fb9
Chris Sewell <chrisj_sewell@hotmail.com>
2023-06-15 13:56:46 +0200

    Update implementation

--- aiida_firecrest/scheduler.py
+++ aiida_firecrest/scheduler.py
@@ -7,8 +7,19 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Scheduler interface."""
+from __future__ import annotations
+
+import re
+import string
+from typing import TYPE_CHECKING
+
+from aiida.engine.processes.exit_code import ExitCode
 from aiida.schedulers import Scheduler
+from aiida.schedulers.datastructures import JobInfo, JobTemplate
 from aiida.schedulers.plugins.slurm import SlurmJobResource
+
+if TYPE_CHECKING:
+    from aiida_firecrest.transport import FirecrestTransport
 
 
 class FirecrestScheduler(Scheduler):
@@ -16,23 +27,171 @@ class FirecrestScheduler(Scheduler):
 
     _job_resource_class = SlurmJobResource
 
-    def _get_submit_script_header(self, job_tmpl):
-        raise NotImplementedError
+    transport: FirecrestTransport
 
-    def _get_joblist_command(self, jobs=None, user=None):
-        raise NotImplementedError
+    def _get_submit_script_header(self, job_tmpl: JobTemplate) -> str:
+        """
+        Return the submit script header, using the parameters from the
+        job_tmpl.
 
-    def _parse_joblist_output(self, retval, stdout, stderr):
-        raise NotImplementedError
+        Args:
+           job_tmpl: an JobTemplate instance with relevant parameters set.
 
-    def _get_submit_command(self, submit_script):
-        raise NotImplementedError
+        TODO: truncate the title if too long
+        """
+        lines = []
+        if job_tmpl.submit_as_hold:
+            lines.append("#SBATCH -H")
 
-    def _parse_submit_output(self, retval, stdout, stderr):
-        raise NotImplementedError
+        if job_tmpl.rerunnable:
+            lines.append("#SBATCH --requeue")
+        else:
+            lines.append("#SBATCH --no-requeue")
 
-    def _get_kill_command(self, jobid):
-        raise NotImplementedError
+        if job_tmpl.email:
+            # If not specified, but email events are set, SLURM
+            # sends the mail to the job owner by default
+            lines.append(f"#SBATCH --mail-user={job_tmpl.email}")
 
-    def _parse_kill_output(self, retval, stdout, stderr):
-        raise NotImplementedError
+        if job_tmpl.email_on_started:
+            lines.append("#SBATCH --mail-type=BEGIN")
+        if job_tmpl.email_on_terminated:
+            lines.append("#SBATCH --mail-type=FAIL")
+            lines.append("#SBATCH --mail-type=END")
+
+        if job_tmpl.job_name:
+            # The man page does not specify any specific limitation
+            # on the job name.
+            # Just to be sure, I remove unwanted characters, and I
+            # trim it to length 128
+
+            # I leave only letters, numbers, dots, dashes and underscores
+            # Note: I don't compile the regexp, I am going to use it only once
+            job_title = re.sub(r"[^a-zA-Z0-9_.-]+", "", job_tmpl.job_name)
+
+            # prepend a 'j' (for 'job') before the string if the string
+            # is now empty or does not start with a valid charachter
+            if not job_title or (
+                job_title[0] not in string.ascii_letters + string.digits
+            ):
+                job_title = f"j{job_title}"
+
+            # Truncate to the first 128 characters
+            # Nothing is done if the string is shorter.
+            job_title = job_title[:128]
+
+            lines.append(f'#SBATCH --job-name="{job_title}"')
+
+        if job_tmpl.import_sys_environment:
+            lines.append("#SBATCH --get-user-env")
+
+        if job_tmpl.sched_output_path:
+            lines.append(f"#SBATCH --output={job_tmpl.sched_output_path}")
+
+        if job_tmpl.sched_join_files:
+            # TODO: manual says:  # pylint: disable=fixme
+            # By  default both standard output and standard error are directed
+            # to a file of the name "slurm-%j.out", where the "%j" is replaced
+            # with  the  job  allocation  number.
+            # See that this automatic redirection works also if
+            # I specify a different --output file
+            if job_tmpl.sched_error_path:
+                self.logger.info(
+                    "sched_join_files is True, but sched_error_path is set in "
+                    "SLURM script; ignoring sched_error_path"
+                )
+        else:
+            if job_tmpl.sched_error_path:
+                lines.append(f"#SBATCH --error={job_tmpl.sched_error_path}")
+            else:
+                # To avoid automatic join of files
+                lines.append("#SBATCH --error=slurm-%j.err")
+
+        if job_tmpl.queue_name:
+            lines.append(f"#SBATCH --partition={job_tmpl.queue_name}")
+
+        if job_tmpl.account:
+            lines.append(f"#SBATCH --account={job_tmpl.account}")
+
+        if job_tmpl.qos:
+            lines.append(f"#SBATCH --qos={job_tmpl.qos}")
+
+        if job_tmpl.priority:
+            #  Run the job with an adjusted scheduling priority  within  SLURM.
+            #  With no adjustment value the scheduling priority is decreased by
+            #  100. The adjustment range is from -10000 (highest  priority)  to
+            #  10000  (lowest  priority).
+            lines.append(f"#SBATCH --nice={job_tmpl.priority}")
+
+        if not job_tmpl.job_resource:
+            raise ValueError(
+                "Job resources (as the num_machines) are required for the SLURM scheduler plugin"
+            )
+
+        lines.append(f"#SBATCH --nodes={job_tmpl.job_resource.num_machines}")
+        if job_tmpl.job_resource.num_mpiprocs_per_machine:
+            lines.append(
+                f"#SBATCH --ntasks-per-node={job_tmpl.job_resource.num_mpiprocs_per_machine}"
+            )
+
+        if job_tmpl.job_resource.num_cores_per_mpiproc:
+            lines.append(
+                f"#SBATCH --cpus-per-task={job_tmpl.job_resource.num_cores_per_mpiproc}"
+            )
+
+        if job_tmpl.max_wallclock_seconds is not None:
+            try:
+                tot_secs = int(job_tmpl.max_wallclock_seconds)
+                if tot_secs <= 0:
+                    raise ValueError
+            except ValueError:
+                raise ValueError(
+                    "max_wallclock_seconds must be "
+                    "a positive integer (in seconds)! It is instead '{}'"
+                    "".format(job_tmpl.max_wallclock_seconds)
+                )
+            days = tot_secs // 86400
+            tot_hours = tot_secs % 86400
+            hours = tot_hours // 3600
+            tot_minutes = tot_hours % 3600
+            minutes = tot_minutes // 60
+            seconds = tot_minutes % 60
+            if days == 0:
+                lines.append(f"#SBATCH --time={hours:02d}:{minutes:02d}:{seconds:02d}")
+            else:
+                lines.append(
+                    f"#SBATCH --time={days:d}-{hours:02d}:{minutes:02d}:{seconds:02d}"
+                )
+
+        # It is the memory per node, not per cpu!
+        if job_tmpl.max_memory_kb:
+            try:
+                physical_memory_kb = int(job_tmpl.max_memory_kb)
+                if physical_memory_kb <= 0:
+                    raise ValueError
+            except ValueError:
+                raise ValueError(
+                    f"max_memory_kb must be a positive integer (in kB)! It is instead `{job_tmpl.max_memory_kb}`"
+                )
+            # --mem: Specify the real memory required per node in MegaBytes.
+            # --mem and  --mem-per-cpu  are  mutually exclusive.
+            lines.append(f"#SBATCH --mem={physical_memory_kb // 1024}")
+
+        if job_tmpl.custom_scheduler_commands:
+            lines.append(job_tmpl.custom_scheduler_commands)
+
+        return "\n".join(lines)
+
+    def submit_job(self, working_directory: str, filename: str) -> str | ExitCode:
+        return self.transport.submit_job(working_directory, filename)
+
+    def get_jobs(
+        self,
+        jobs: list[str] | None = None,
+        user: str | None = None,
+        as_dict: bool = False,
+    ) -> list[JobInfo] | dict[str, JobInfo]:
+        return self.transport.get_jobs(jobs, user, as_dict)
+
+    def kill_job(self, jobid: str) -> bool:
+        return self.transport.kill_job(jobid)
--- aiida_firecrest/transport.py
+++ aiida_firecrest/transport.py
@@ -7,23 +7,28 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Transport interface."""
+from __future__ import annotations
+
 import os
 import posixpath
 from pathlib import Path, PurePosixPath
-from typing import Any, List, NamedTuple, Optional, Tuple, Type, TypedDict, Union
+from typing import Any, NamedTuple, TypedDict
 
 import firecrest as f7t
 from aiida.cmdline.params.options.overridable import OverridableOption
 from aiida.cmdline.params.types.path import AbsolutePathOrEmptyParamType
+from aiida.engine.processes.exit_code import ExitCode
+from aiida.schedulers.datastructures import JobInfo, JobState
+from aiida.schedulers.plugins.slurm import _MAP_STATUS_SLURM
 from aiida.transports import Transport
 from click.types import ParamType
 from firecrest.FirecrestException import HeaderException
 
 
 class ValidAuthOption(TypedDict, total=False):  # type: ignore
-    option: Optional[OverridableOption]  # existing option
+    option: OverridableOption | None  # existing option
     switch: bool  # whether the option is a boolean flag
-    type: Union[Type[Any], ParamType]  # noqa: A003
+    type: type[Any] | ParamType  # noqa: A003
     default: Any
     non_interactive_default: bool  # whether option should provide a default in non-interactive mode
     prompt: str  # for interactive CLI
@@ -39,7 +44,7 @@ def login_decorator(func):
 
 class FirecrestAuthorizationClass:
     def __init__(self, token_uri: str, client_id: str, client_secret: str):
-        self.keycloak = f7t.ClientCredentialsAuthorization(
+        self.keycloak = f7t.ClientCredentialsAuth(
             client_id,
             client_secret,
             token_uri,
@@ -66,7 +71,7 @@ class StatResult(NamedTuple):
 class FirecrestTransport(Transport):
     """Transport interface for FirecREST."""
 
-    _valid_auth_options: List[Tuple[str, dict]] = [
+    _valid_auth_options: list[tuple[str, dict]] = [
         (
             "url",
             {
@@ -112,7 +117,7 @@ class FirecrestTransport(Transport):
         url: str,
         token_uri: str,
         client_id: str,
-        client_secret: Union[str, Path],
+        client_secret: str | Path,
         machine: str,
         **kwargs: Any,
     ):
@@ -137,9 +142,9 @@ class FirecrestTransport(Transport):
 
         self._cwd = PurePosixPath()
 
-    def _get_path(self, path: str) -> str:
+    def _get_path(self, *path: str) -> str:
         # TODO ensure all remote paths are manipulated with posixpath
-        return posixpath.normpath(self._cwd.joinpath(path))
+        return posixpath.normpath(self._cwd.joinpath(*path))
 
     def open(self):  # noqa: A003
         # TODO allow for batch connections in pyfirecrest?
@@ -203,7 +208,7 @@ class FirecrestTransport(Transport):
 
     def _convert_st_mode(self, data: dict) -> int:
         # created from `ls -l -A --time-style=+%Y-%m-%dT%H:%M:%S`, e.g.
-        # -rw-------  1 chrisjsewell staff 57 2021-12-02T10:42:00 file.txt
+        # -rw-------  1 username usergroup 57 2021-12-02T10:42:00 file.txt
         # type, permissions, # of links (not recorded), user, group, size, last_modified, name
 
         ftype = {
@@ -233,7 +238,7 @@ class FirecrestTransport(Transport):
         # TODO lstat
         dirname, filename = posixpath.split(self._get_path(path))
         try:
-            output = self._client.list_files(self._machine, dirname, showhidden=True)
+            output = self._client.list_files(self._machine, dirname, show_hidden=True)
         except HeaderException as exc:
             if "X-Invalid-Path" in exc.responses[-1].headers:
                 raise FileNotFoundError(posixpath.join(dirname, filename))
@@ -265,12 +270,12 @@ class FirecrestTransport(Transport):
             raise
         return ftype != "directory"
 
-    def listdir(self, path: str = ".", pattern: Optional[str] = None) -> List[str]:
+    def listdir(self, path: str = ".", pattern: str | None = None) -> list[str]:
         if pattern is not None:
             raise NotImplementedError("pattern matching")
         try:
             output = self._client.list_files(
-                self._machine, self._get_path(path), showhidden=True
+                self._machine, self._get_path(path), show_hidden=True
             )
         except HeaderException as exc:
             if "X-Invalid-Path" in exc.responses[-1].headers:
@@ -318,7 +323,7 @@ class FirecrestTransport(Transport):
         # note this allows overwriting
         self._client.simple_upload(self._machine, localpath, str(remotepathlib.parent))
 
-    def puttree(self, localpath: Union[str, Path], remotepath: str, *args, **kwargs):
+    def puttree(self, localpath: str | Path, remotepath: str, *args, **kwargs):
         localpath = Path(localpath)
 
         # local checks
@@ -345,9 +350,7 @@ class FirecrestTransport(Transport):
         self._client.simple_delete(self._machine, self._get_path(path))
 
     def rename(self, oldpath: str, newpath: str):
-        self._client.rename(
-            self._machine, self._get_path(oldpath), self._get_path(newpath)
-        )
+        self._client.mv(self._machine, self._get_path(oldpath), self._get_path(newpath))
 
     def rmdir(self, path: str):
         self._client.simple_delete(self._machine, self._get_path(path))
@@ -370,3 +373,173 @@ class FirecrestTransport(Transport):
 
     def exec_command_wait_bytes(self, command, stdin=None, **kwargs):
         raise NotImplementedError
+
+    # pass through from scheduler
+
+    def submit_job(self, working_directory: str, filename: str) -> str | ExitCode:
+        """Submit a job.
+
+        :param working_directory: The absolute filepath to the working directory where the job is to be executed.
+        :param filename: The filename of the submission script relative to the working directory.
+        :return: a string with the job ID or an exit code if the submission failed because the submission script is
+            invalid and the job should be terminated.
+        """
+        result = self._client.submit(
+            self._machine, self._get_path(working_directory, filename), local_file=False
+        )
+        return str(result["jobid"])
+
+    def get_jobs(
+        self,
+        jobs: list[str] | None = None,
+        user: str | None = None,
+        as_dict: bool = False,
+    ) -> list[JobInfo] | dict[str, JobInfo]:
+        """Return the list of currently active jobs.
+
+        :param jobs: A list of jobs to check; only these are checked.
+        :param user: A string with a user: only jobs of this user are checked.
+        :param as_dict: If False (default), a list of JobInfo objects is returned. If True, a dictionary is
+            returned, where the job_id is the key and the values are the JobInfo objects.
+        :returns: List of active jobs.
+        """
+        result = self._client.poll_active(self._machine, jobs)
+        job_list = []
+        for raw_result in result:
+
+            if user is not None and raw_result["user"] != user:
+                continue
+
+            this_job = JobInfo()
+
+            this_job.job_id = raw_result["jobid"]
+            this_job.annotation = ""
+
+            job_state_raw = raw_result["state"]
+
+            try:
+                job_state_string = _MAP_STATUS_SLURM[job_state_raw]
+            except KeyError:
+                self.logger.warning(
+                    f"Unrecognized job_state '{job_state_raw}' for job id {this_job.job_id}"
+                )
+                job_state_string = JobState.UNDETERMINED
+            # QUEUED_HELD states are not specific states in SLURM;
+            # they are instead set with state QUEUED, and then the
+            # annotation tells if the job is held.
+            # I check for 'Dependency', 'JobHeldUser',
+            # 'JobHeldAdmin', 'BeginTime'.
+            # Other states should not bring the job in QUEUED_HELD, I believe
+            # (the man page of slurm seems to be incomplete, for instance
+            # JobHeld* are not reported there; I also checked at the source code
+            # of slurm 2.6 on github (https://github.com/SchedMD/slurm),
+            # file slurm/src/common/slurm_protocol_defs.c,
+            # and these seem all the states to be taken into account for the
+            # QUEUED_HELD status).
+            # There are actually a few others, like possible
+            # failures, or partition-related reasons, but for the moment I
+            # leave them in the QUEUED state.
+            if job_state_string == JobState.QUEUED and this_job.annotation in [
+                "Dependency",
+                "JobHeldUser",
+                "JobHeldAdmin",
+                "BeginTime",
+            ]:
+                job_state_string = JobState.QUEUED_HELD
+
+            this_job.job_state = job_state_string
+
+            # The rest is optional
+
+            this_job.job_owner = raw_result["user"]
+
+            try:
+                this_job.num_machines = int(raw_result["nodes"])
+            except ValueError:
+                self.logger.warning(
+                    "The number of allocated nodes is not "
+                    "an integer ({}) for job id {}!".format(
+                        raw_result["nodes"], this_job.job_id
+                    )
+                )
+
+            # try:
+            #     this_job.num_mpiprocs = int(thisjob_dict['number_cpus'])
+            # except ValueError:
+            #     self.logger.warning(
+            #         'The number of allocated cores is not '
+            #         'an integer ({}) for job id {}!'.format(thisjob_dict['number_cpus'], this_job.job_id)
+            #     )
+
+            # ALLOCATED NODES HERE
+            # string may be in the format
+            # nid00[684-685,722-723,748-749,958-959]
+            # therefore it requires some parsing, that is unnecessary now.
+            # I just store is as a raw string for the moment, and I leave
+            # this_job.allocated_machines undefined
+            # if this_job.job_state == JobState.RUNNING:
+            #     this_job.allocated_machines_raw = thisjob_dict['allocated_machines']
+
+            this_job.queue_name = raw_result["partition"]
+
+            # try:
+            #     walltime = (self._convert_time(thisjob_dict['time_limit']))
+            #     this_job.requested_wallclock_time_seconds = walltime  # pylint: disable=invalid-name
+            # except ValueError:
+            #     self.logger.warning(f'Error parsing the time limit for job id {this_job.job_id}')
+
+            # Only if it is RUNNING; otherwise it is not meaningful,
+            # and may be not set (in my test, it is set to zero)
+            # if this_job.job_state == JobState.RUNNING:
+            #     try:
+            #         this_job.wallclock_time_seconds = (self._convert_time(thisjob_dict['time_used']))
+            #     except ValueError:
+            #         self.logger.warning(f'Error parsing time_used for job id {this_job.job_id}')
+
+            #     try:
+            #         this_job.dispatch_time = self._parse_time_string(thisjob_dict['dispatch_time'])
+            #     except ValueError:
+            #         self.logger.warning(f'Error parsing dispatch_time for job id {this_job.job_id}')
+
+            # try:
+            #     this_job.submission_time = self._parse_time_string(thisjob_dict['submission_time'])
+            # except ValueError:
+            #     self.logger.warning(f'Error parsing submission_time for job id {this_job.job_id}')
+
+            this_job.title = raw_result["name"]
+
+            # Everything goes here anyway for debugging purposes
+            this_job.raw_data = raw_result
+
+            # Double check of redundant info
+            # Not really useful now, allocated_machines in this
+            # version of the plugin is never set
+            if (
+                this_job.allocated_machines is not None
+                and this_job.num_machines is not None
+            ):
+                if len(this_job.allocated_machines) != this_job.num_machines:
+                    self.logger.error(
+                        "The length of the list of allocated "
+                        "nodes ({}) is different from the "
+                        "expected number of nodes ({})!".format(
+                            len(this_job.allocated_machines), this_job.num_machines
+                        )
+                    )
+
+            # I append to the list of jobs to return
+            job_list.append(this_job)
+
+        if as_dict:
+            return {job.job_id: job for job in job_list}
+
+        return job_list
+
+    def kill_job(self, jobid: str) -> bool:
+        """Kill a job with a given job ID.
+
+        :param jobid: The job ID of the job to kill.
+        :return: True if the job was successfully killed, False otherwise.
+        """
+        self._client.cancel(self._machine, jobid)
+        return True
