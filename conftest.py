c53f28250c2387770f3195d72870cf87e43ca6e0
Ali <khsrali@gmail.com>
2025-06-18 12:54:07 +0200

    fix licence header and version hooked

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
-###########################################################################
-# Copyright (c), The AiiDA team. All rights reserved.                     #
-# This file is part of the AiiDA code.                                    #
-#                                                                         #
-# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
-# For further information on the license, see the LICENSE.txt file        #
-# For further information please visit http://www.aiida.net               #
-###########################################################################
+################################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                          #
+# This file is part of the AiiDA code.                                         #
+#                                                                              #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-firecrest #
+# For further information on the license, see the LICENSE.txt file             #
+# For further information please visit http://www.aiida.net                    #
+################################################################################
 """Pytest configuration that must be at the root level."""
 
 pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]
